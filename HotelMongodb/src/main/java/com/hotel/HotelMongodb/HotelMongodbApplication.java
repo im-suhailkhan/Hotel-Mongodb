9bab91ae0c77e50c0cf48a3bf700db949ea5965d
suhailkhanqbox <suhail.khan.ftg@gmail.com>
2021-05-08 14:13:56 +0530

    spring intilzer added mongodb,lombok and webstarter dependency

---
+++ HotelMongodb/src/main/java/com/hotel/HotelMongodb/HotelMongodbApplication.java
@@ -0,0 +1,13 @@
+package com.hotel.HotelMongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelMongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelMongodbApplication.class, args);
+	}
+
+}
